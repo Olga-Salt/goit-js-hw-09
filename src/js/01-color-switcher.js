06cb72938f786615cc9a875173fec83a770c5c30
Olga Saltanovskaya <saltanoffskaya@gmail.com>
2021-11-05 19:19:34 +0200

    create color-switcher
    * Сhanges the color of the body to a random color when the start button is pressed
    
    * The color changes once a second
    
    * When the stop button is active, the start button is disabled and exactly the opposite

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  body: document.body,
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
 
+let INTERVAL_ID = null;
+
+refs.btnStop.setAttribute('disabled', true);
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  INTERVAL_ID = setInterval(() => {
+    const randomColor = (refs.body.style.backgroundColor = getRandomHexColor());
+  }, 1000);
+  refs.btnStart.setAttribute('disabled', true);
+  refs.btnStop.removeAttribute('disabled');
+}
+
+function onBtnStopClick() {
+  clearInterval(INTERVAL_ID);
+  refs.btnStart.removeAttribute('disabled');
+  refs.btnStop.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
